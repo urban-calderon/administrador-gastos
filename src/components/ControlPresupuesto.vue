f5d7e3962a07dcc75f09c02a5b9317fdb6849371
Urbano Calderón Jiménez <103982150+urban-calderon@users.noreply.github.com>
2024-10-03 16:08:26 -0600

    add: nuevo componente para mostrar el control de presupuesto

---
+++ src/components/ControlPresupuesto.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="dos-columnas">
+    <div class="contenedor-grafico">
+      <img :src="imagen" alt="" />
+    </div>
+
+    <div class="contenedor-presupuesto">
+      <button class="reset-app">Resetear App</button>
+      <p>
+        <span>Presupuesto:</span>
+        {{ formatearCantidad(presupuesto) }}
+      </p>
+
+      <p>
+        <span>Disponible:</span>
+        {{ formatearCantidad(disponible) }}
+      </p>
+
+      <p>
+        <span>Gastado:</span>
+        {{ formatearCantidad(gastado) }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { formatearCantidad } from "../helpers";
+import imagen from "../assets/img/grafico.jpg";
+
+const props = defineProps({
+  presupuesto: {
+    type: Number,
+    required: true,
+  },
+  disponible: {
+    type: Number,
+    required: true,
+  },
+  gastado: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.dos-columnas {
+  display: flex;
+  flex-direction: column;
+}
+
+.dos-columnas > :first-child {
+  margin-bottom: 3rem;
+}
+
+@media (min-width: 768px) {
+  .dos-columnas {
+    flex-direction: row;
+    gap: 4rem;
+    align-items: center;
+  }
+
+  .dos-columnas > :first-child {
+    margin-bottom: 0;
+  }
+}
+
+.contenedor-presupuesto {
+  width: 100%;
+}
+
+.contenedor-presupuesto p {
+  font-size: 2.4rem;
+  text-align: center;
+  color: var(--gris-oscuro);
+}
+
+@media (min-width: 768px) {
+  .contenedor-presupuesto p {
+    text-align: left;
+  }
+}
+
+.contenedor-presupuesto span {
+  font-weight: 900;
+  color: var(--azul);
+}
+
+.reset-app {
+  background-color: #db2777;
+  border: none;
+  padding: 1rem;
+  width: 100%;
+  color: var(--blanco);
+  font-weight: 900;
+  text-transform: uppercase;
+  border-radius: 1rem;
+  transition-property: background-color;
+  transition-duration: 300ms;
+}
+
+.reset-app:hover {
+  cursor: pointer;
+  background-color: #c11d67;
+}
+</style>
