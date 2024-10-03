cf1371cd1feb6db5772e418f82ce4f7a903d388a
Urbano Calderón Jiménez <103982150+urban-calderon@users.noreply.github.com>
2024-10-03 15:56:50 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,269 @@
+<template>
+  <div :class="{ fijar: modal.mostrar }">
+    <header>
+      <h1>Planificador de Gastos</h1>
+      <div class="contenedor-header contenedor sombra">
+        <Presupuesto
+          v-if="presupuesto === 0"
+          @definir-presupuesto="definirPresupuesto"
+        />
+        <ControlPresupuesto
+          :presupuesto="presupuesto"
+          :disponible="disponible"
+          :gastado="gastado"
+          v-else
+        />
+      </div>
+    </header>
+    <main v-if="presupuesto > 0">
+      <div class="listado-gastos contenedor">
+        <h2>{{ gastos.length > 0 ? "Gastos" : "No hay gastos" }}</h2>
+
+        <Gasto
+          v-for="gasto in gastos"
+          :key="gasto.id"
+          :gasto="gasto"
+          @seleccionar-gasto="seleccionarGasto"
+        />
+      </div>
+      <div class="crear-gasto">
+        <img
+          :src="iconoNuevoGasto"
+          alt="Icono nuevo gasto"
+          @click="mostrarModal"
+        />
+      </div>
+      <Modal
+        v-if="modal.mostrar"
+        @ocultar-modal="ocultarModal"
+        @guardar-gasto="guardarGasto"
+        :modal="modal"
+        :disponible="disponible"
+        v-model:nombre="gasto.nombre"
+        v-model:cantidad="gasto.cantidad"
+        v-model:categoria="gasto.categoria"
+      />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive, watch } from "vue";
+import { generarId, formatearFecha } from "./helpers";
+import Modal from "./components/Modal.vue";
+import Presupuesto from "./components/Presupuesto.vue";
+import ControlPresupuesto from "./components/ControlPresupuesto.vue";
+import Gasto from "./components/Gasto.vue";
+import iconoNuevoGasto from "./assets/img/nuevo-gasto.svg";
+
+const presupuesto = ref(0);
+const disponible = ref(0);
+const gastado = ref(0);
+
+const modal = reactive({
+  mostrar: false,
+  animar: false,
+});
+
+const gasto = reactive({
+  nombre: "",
+  cantidad: "",
+  categoria: "",
+  id: null,
+  fecha: Date.now(),
+});
+const gastos = ref([
+  /* {
+    nombre: "Netflix",
+    cantidad: 20,
+    categoria: "Subscripciones",
+    id: "bfghgfgfhfgh",
+    fecha: Date.now(),
+  }, */
+]);
+
+watch(
+  gastos,
+  () => {
+    const totalGastado = gastos.value.reduce(
+      (total, gasto) => gasto.cantidad + total,
+      0
+    );
+    gastado.value = totalGastado;
+    disponible.value = presupuesto.value - gastado.value;
+  },
+  {
+    deep: true,
+  }
+);
+
+//** Solución 2: limpiar formulario cada vez que se cierre el modal */
+watch(
+  modal,
+  () => {
+    if (!modal.mostrar) {
+      reiniciarStateGasto();
+    }
+  },
+  {
+    deep: true,
+  }
+);
+
+const definirPresupuesto = (cantidad) => {
+  presupuesto.value = cantidad;
+  disponible.value = cantidad;
+};
+
+const mostrarModal = () => {
+  modal.mostrar = true;
+  setTimeout(() => {
+    modal.animar = true;
+  }, 300);
+};
+
+const ocultarModal = () => {
+  modal.animar = false;
+  setTimeout(() => {
+    modal.mostrar = false;
+  }, 300);
+
+  //** Solución 1: limpiar formulario cada vez que se cierre el modal */
+  //reiniciarStateGasto();
+};
+
+const reiniciarStateGasto = () => {
+  Object.assign(gasto, {
+    nombre: "",
+    cantidad: "",
+    categoria: "",
+    id: null,
+    fecha: Date.now(),
+  });
+};
+
+const guardarGasto = () => {
+  if (gasto.id) {
+    //editamos
+    const { id } = gasto;
+    const i = gastos.value.findIndex((gasto) => gasto.id === id);
+    gastos.value[i] = { ...gasto };
+  } else {
+    //guardamos un registro nuevo
+    gastos.value.push({
+      ...gasto,
+      id: generarId(),
+      //fecha: formatearFecha(date),
+    });
+  }
+
+  ocultarModal();
+  //Limpiar formulario
+  reiniciarStateGasto();
+};
+
+const validarGasto = () => {
+  const existe = gastos.value.find((element, index) => {
+    return element.nombre === gasto.nombre;
+  });
+  //console.log(existe);
+  if (!existe) {
+    gastos.value.push({
+      ...gasto,
+      id: generarId(),
+      //fecha: formatearFecha(date),
+    });
+
+    ocultarModal();
+
+    //Limpiar formulario
+    reiniciarStateGasto();
+  } else {
+    existe.cantidad += gasto.cantidad;
+    ocultarModal();
+    reiniciarStateGasto();
+  }
+};
+
+const seleccionarGasto = (id) => {
+  const gastoEditar = gastos.value.filter((gasto) => gasto.id === id)[0];
+  console.log(gastoEditar);
+  Object.assign(gasto, gastoEditar);
+  mostrarModal();
+};
+</script>
+
+<style>
+:root {
+  --azul: #3b82f6;
+  --blanco: #fff;
+  --gris-claro: #dadadb;
+  --gris-oscuro: #64748b;
+  --negro: #000;
+}
+html {
+  font-size: 62.5%;
+  box-sizing: border-box;
+}
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+body {
+  font-size: 1.6rem;
+  font-family: "Lato", sans-serif;
+  background-color: var(--gris-claro);
+}
+h1 {
+  font-size: 4rem;
+}
+h2 {
+  font-size: 3rem;
+}
+.fijar {
+  overflow: hidden;
+  height: 100vh;
+}
+header {
+  background-color: var(--azul);
+}
+header h1 {
+  padding: 3rem 0;
+  margin: 0;
+  color: var(--blanco);
+  text-align: center;
+}
+.contenedor {
+  width: 90%;
+  max-width: 80rem;
+  margin: 0 auto;
+}
+.contenedor-header {
+  margin-top: -5rem;
+  transform: translateY(5rem);
+  padding: 5rem;
+}
+.sombra {
+  box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+  background-color: var(--blanco);
+  border-radius: 1.2rem;
+  padding: 5rem;
+}
+.crear-gasto {
+  position: fixed;
+  bottom: 5rem;
+  right: 5rem;
+}
+.crear-gasto img {
+  width: 5rem;
+  cursor: pointer;
+}
+.listado-gastos {
+  margin-top: 10rem;
+}
+.listado-gastos h2 {
+  font-weight: 900;
+  color: var(--gris-oscuro);
+}
+</style>
