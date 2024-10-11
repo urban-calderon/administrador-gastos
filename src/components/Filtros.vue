10f7cdf4770837c62b1c32bdde6063d8752c2420
Urbano Calderón Jiménez <103982150+urban-calderon@users.noreply.github.com>
2024-10-11 16:12:50 -0600

    add: componente de filtro

---
+++ src/components/Filtros.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="filtros sombra contenedor">
+    <form>
+      <div class="campo">
+        <label for="filtro">Filtrar Gastos</label>
+        <select
+          id="filtro"
+          :value="filtro"
+          @input="$emit('update:filtro', $event.target.value)"
+        >
+          <option value="">-- Seleccione --</option>
+          <option value="ahorro">Ahorro</option>
+          <option value="comida">Comida</option>
+          <option value="casa">Casa</option>
+          <option value="gastos">Gastos</option>
+          <option value="ocio">Ocio</option>
+          <option value="salud">Salud</option>
+          <option value="suscripciones">Suscripciones</option>
+        </select>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+defineEmits(["update:filtro"]);
+const props = defineProps({
+  filtro: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.filtros {
+  margin-top: 10rem;
+}
+
+.filtros .campo {
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+}
+
+.filtros label {
+  font-size: 3rem;
+  font-weight: 900;
+  color: var(--gris-oscuro);
+}
+
+.filtros select {
+  flex: 1;
+  padding: 1rem;
+  border: none;
+  border-radius: 1rem;
+  background-color: var(--gris-claro);
+  text-align: center;
+}
+
+@media (max-width: 600px) {
+  .filtros label {
+    font-size: 2rem;
+  }
+}
+</style>
